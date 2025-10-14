a184e10e6f3fc090b9af7e01842fe866061f8703
vpq5kd <vpq5kd@virginia.edu>
2025-10-14 17:41:20 +0000

    updated mylib.h

--- mylib.h
+++ mylib.h
@@ -12,7 +12,7 @@ double magnitude(double re, double im);
   int mandel_test(double c_re, double c_im, int NTRIALS);
 void mandel(double *img, double re1, double re2, double im1, double im2,
 	    int nr, int ni, int NTRIALS=255);
-  
+double HSVolume(int d, long long N, double r);  
 #ifdef EXTERNC
 }
 #endif
